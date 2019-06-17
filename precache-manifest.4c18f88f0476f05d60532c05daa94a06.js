603ff963c70007fee7669d3eab95fb08929f976b
pavelsalfetkin <pavelsalf@yandex.ru>
2019-06-17 21:05:14 +0300

    Updates

---
+++ precache-manifest.4c18f88f0476f05d60532c05daa94a06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e403caad8b6f7406580eea9b9a624b9e",
+    "url": "/monitor-soft-test/index.html"
+  },
+  {
+    "revision": "b14df4ef39d97a9bd188",
+    "url": "/monitor-soft-test/static/css/2.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "8d8260d2ca38ba684c1e",
+    "url": "/monitor-soft-test/static/css/main.3777f7a0.chunk.css"
+  },
+  {
+    "revision": "b14df4ef39d97a9bd188",
+    "url": "/monitor-soft-test/static/js/2.57978956.chunk.js"
+  },
+  {
+    "revision": "8d8260d2ca38ba684c1e",
+    "url": "/monitor-soft-test/static/js/main.95c23b5e.chunk.js"
+  },
+  {
+    "revision": "32e1516f06d09cd58996",
+    "url": "/monitor-soft-test/static/js/runtime~main.e35efefb.js"
+  }
+]);
